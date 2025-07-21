78f25529f2c7239b28cf4f20bf0e035187a23be0
emorizvi <emonrizvi10@gmail.com>
2025-07-21 09:40:19 +0100

    A basic chatbot that talks about your mood

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello, I am your friendly AI chatbot")
+
+name = input("What is your name? ")
+
+print(f"Hello, {name} how are you (good/bad)")
+
+mood = input().lower()
+
+if mood == "good":
+    print("It's nice to hear that")
+elif mood == "bad":
+    print("Sorry to hear that, hope you feel better soon")
+else:
+    print("Sometimes it is hard to put feeling into words")
+
+print(f"Anyways, it was nice getting to know you, {name}")
+print("Goodbye!")
